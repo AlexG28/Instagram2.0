786c48f34bc9b1cec285888ad3527210a6920e03
Alex <alex.gyumushyan@gmail.com>
2022-12-06 23:11:53 -0500

    testing new cli pipeline 7.1

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW")
+console.log(supabaseUrl)
+console.log(supabaseAnonKey)
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
